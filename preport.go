58638f7132dddc814f18406fb5844c0fc9d2be91
Emile Pels <mail@emilepels.nl>
2021-11-20 21:38:14 +0100

    Add PullRequestsByCreatedAt sorter

--- preport.go
+++ preport.go
@@ -13,3 +13,11 @@ type PullRequest struct {
 type Author struct {
 	Username string
 }
+
+// PullRequestsBySortedAt providers a sorter based on CreatedAt timestamp, from
+// oldest to newest.
+type PullRequestsByCreatedAt []PullRequest
+
+func (ps PullRequestsByCreatedAt) Len() int           { return len(ps) }
+func (ps PullRequestsByCreatedAt) Swap(i, j int)      { ps[i], ps[j] = ps[j], ps[i] }
+func (ps PullRequestsByCreatedAt) Less(i, j int) bool { return ps[i].CreatedAt.Before(ps[j].CreatedAt) }
